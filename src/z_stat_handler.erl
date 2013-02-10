2b3ec0559d65a0b15f67be324e1d44807d86451b
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-02-10 15:56:21 +0100

    Api change

--- src/z_stat_handler.erl
+++ src/z_stat_handler.erl
@@ -11,22 +11,18 @@
 %% Initialize folsom.
 %% 
 init() ->
-    ok
-   
-% @doc
-% 
-update(#counter{name=Name, op='incr', value=V}, #stats_from{host=Host, system=System}) ->
-	folsom_metrics:notify({Host, System, Name}, {inc, V});
-update(#counter{name=Name, op='decr', value=V}, #stats_from{host=Host, system=System}) ->
-    folsom_metrics:notify({Host, System, Name}, {dec, V});
-update(#counter{name=Name, op='set', value=V}, #stats_from{host=Host, system=System}) ->
-    ok;
+	folsom:start().
+    
+% counter
+update(#counter{name=Name, op=incr, value=Value}, #stats_from{host=Host, system=System}) ->
+	folsom_metrics:notify({Host, System, Name}, {inc, Value});
+update(#counter{name=Name, op=decr, value=Value}, #stats_from{host=Host, system=System}) ->
+    folsom_metrics:notify({Host, System, Name}, {dec, Value});
+update(#counter{name=Name, op=clear, value=Value}, #stats_from{host=Host, system=System}) ->
+    folsom_metrics:notify({Host, System, Name}, clear);
+
+% histogram
 update(#histogram{name=Name, value=Value}, #stats_from{host=Host, system=System}) ->
     folsom_metrics:notify({{Host, System, Name}, Value}).
-    
-update(_Stat, []) ->
-    ok;
-update(Stat, [H|T]) ->
-    update(Stat, H),
-    update(Stat, T).
+  
     
